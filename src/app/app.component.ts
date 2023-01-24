3225458c25f2a72c0e6435a417d33e790a33f126
ElizabethKuriakose <74717020+ElizabethKuriakose@users.noreply.github.com>
2023-01-24 14:18:08 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { TranslateService } from '@ngx-translate/core';
+import { Constants } from './configs/app.config';
 
 @Component({
-  selector: 'app-root',
+  selector: 'ion-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'IonCareer';
+export class AppComponent implements OnInit {
+  
+  constructor(private router:Router,private appConstants:Constants,
+    private translateService:TranslateService){
+    
+    this.translateService.use(appConstants.defaultLanguage)
+    
+  }
+  ngOnInit(){
+    this.router.navigate(['/home'])
+  }
 }
